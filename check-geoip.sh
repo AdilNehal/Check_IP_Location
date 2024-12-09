84d79e8b7c030225dfdacde1b9128975be68713f
Adil Nehal <adilnehal2000@gmail.com>
2024-12-09 17:15:54 +0000

    practice bash

---
+++ check-geoip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Counting number of failed login attempts from an IP
+
+LIMIT='10'
+LOG_FILE="${1}"
+
+#Making sure file is supplied
+if [[ ! -e "$LOG_FILE"  ]];then
+	echo "can't open log file ${LOG_FILE}" >&2
+	exit 1
+fi
+
+cat syslog-sample | grep -i Failed | awk -F 'from ' '{print $2}' | awk '{print $1}' | sort | uniq -c | sort -n | while read COUNT IP
+do
+	if [[ "${COUNT}" -gt "$LIMIT" ]]
+	then
+		LOCATION=$(geoiplookup ${IP})
+		echo "${COUNT} ${IP} ${LOCATION}"
+	fi
+done
+exit 0

75a5c56bfbe821351a926efc79b2988be032de90
Adil Nehal <adilnehal2000@gmail.com>
2024-12-09 17:26:00 +0000

    practice bash

--- check-geoip.sh
+++ check-geoip.sh
@@ -11,12 +11,15 @@ if [[ ! -e "$LOG_FILE"  ]];then
 	exit 1
 fi
 
+#CSV header
+echo 'Count,IP,Location'
+
 cat syslog-sample | grep -i Failed | awk -F 'from ' '{print $2}' | awk '{print $1}' | sort | uniq -c | sort -n | while read COUNT IP
 do
 	if [[ "${COUNT}" -gt "$LIMIT" ]]
 	then
-		LOCATION=$(geoiplookup ${IP})
-		echo "${COUNT} ${IP} ${LOCATION}"
+		LOCATION=$(geoiplookup ${IP} | awk -F ', ' '{print $2}')
+		echo "${COUNT},${IP},${LOCATION}"
 	fi
 done
 exit 0
